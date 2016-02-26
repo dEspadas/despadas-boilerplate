b9e3c0b2f3a75fbd2baa112801167790dce21141
MarcusFeital <marcusfeital@despadas.com>
2016-02-25 22:25:07 -0300

    Added Helmet; Added Redbird; Added Serve-Static.

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var Express = require('express');
 var app  = Express();
+var serveStatic = require('serve-static');
+var helmet = require('helmet');
 
 //Dev server config
-app.use(Express.static('./src'));
+//app.use(Express.static('./src'));
 
-app.get('/', function (req, res) {
-  //Send index in Dev config
-  res.sendfile('./src/index.html');
-});
+//app.get('/', function (req, res) {
+//  Send index in Dev config
+//  res.sendfile('./src/index.html');
+//});
+
+app.use(helmet());
+app.use(serveStatic('src', { 'index': ['index.html', 'index.htm'] }));
 
 app.listen(3000);
---
+++ proxy.js
@@ -0,0 +1,3 @@
+var proxy = require('redbird')({ port: 80, xfwd: false });
+
+proxy.register('localhost', 'http://localhost:3000');
