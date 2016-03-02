bfee20fe245f10648827128315566f6b320b5c60
MarcusFeital <marcusfeital@despadas.com>
2016-03-02 00:47:11 -0300

    Improved gulp (wiredep, merge-stream); improved express code and routing; added vash; improved edior (ESLint, Bootlint)

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,55 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+var wiredep = require('wiredep')
+var merge = require('merge-stream')
 
-gulp.task('copy-bootstrap', function () {
-  return gulp.src('./bower_components/bootstrap/dist/**/*', { base: './bower_components/bootstrap/dist/' })
-             .pipe(gulp.dest('./src/'));
-});
+var config = {
+  dev: {
+    host: 'http://localhost:3000/'
+  }
+}
 
-gulp.task('copy-bootstrap-material', function () {
-  return gulp.src('./bower_components/bootstrap-material-design/dist/**/*', { base: './bower_components/bootstrap-material-design/dist/' })
-             .pipe(gulp.dest('./src/'));
-});
+// CSS and JS bower file injection to temp dir
+// http://lab.brightnorth.co.uk/2014/08/13/automating-linkage-how-i-learned-to-stop-worrying-and-love-the-build/
+gulp.task('index-build', ['libs-build', 'bootstrap-font', 'bootstrap-examples'], function () {
+  console.log('Injecting index.html with the bower files...')
+
+  return gulp.src('src/views/layout.vash')
+              .pipe(wiredep.stream({
+                fileTypes: {
+                  html: {
+                    replace: {
+                      js: function (filePath) {
+                        return '<script src="' + config.dev.host + 'libs/' + filePath.split('/').pop() + '"></script>'
+                      },
+                      css: function (filePath) {
+                        return '<link rel="stylesheet" href="' + config.dev.host + 'css/' + filePath.split('/').pop() + '"/>'
+                      }
+                    }
+                  }
+                }
+              }))
+              .pipe(gulp.dest('./tmp/views/'))
+})
+
+gulp.task('libs-build', function () {
+  console.log('Copying bower files to tmp directory...')
+
+  var vendorScripts = gulp.src(wiredep().js).pipe(gulp.dest('./tmp/libs'))
+  var vendorCss = gulp.src(wiredep().css).pipe(gulp.dest('./tmp/css'))
+
+  return merge(vendorScripts, vendorCss)
+})
+
+gulp.task('bootstrap-font', function () {
+  console.log('Copying bootstrap font files to tmp directory...')
+
+  return gulp.src('./bower_components/bootstrap/dist/fonts/*.*', { base: './bower_components/bootstrap/dist/' })
+             .pipe(gulp.dest('./tmp/'))
+})
+
+gulp.task('bootstrap-examples', function () {
+  console.log('Copying bootstrap example files to tmp directory...')
+
+  return gulp.src(['./src/views/templates/**/*.vash', './src/views/*.vash', '!./src/views/layout.vash'], { base: './src/' })
+             .pipe(gulp.dest('./tmp/'))
+})
--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-var Express = require('express');
-var app  = Express();
-var serveStatic = require('serve-static');
-var helmet = require('helmet');
+var express = require('express')
+// var serveStatic = require('serve-static')
+var helmet = require('helmet')
+var path = require('path')
 
-//Dev server config
-//app.use(Express.static('./src'));
+var app = express()
 
-//app.get('/', function (req, res) {
-//  Send index in Dev config
-//  res.sendfile('./src/index.html');
-//});
+app.set('views', path.join(__dirname, 'tmp/views')) // critical to use path.join on windows
+app.set('view engine', 'vash')
 
-app.use(helmet());
-app.use(serveStatic('src', { 'index': ['index.html', 'index.htm'] }));
+// Routes
+app.get('/', function (req, res) {
+  res.render('index', {
+    channel: 'Express',
+    title: 'dEspadas Boilerplate'
+  })
+})
+app.get('/templates/bootstrap/theme', function (req, res) {
+  res.render('templates/bootstrap/theme/index', {
+    channel: 'Express',
+    title: 'dEspadas Boilerplate - bootstrap templates'
+  })
+})
+app.get('/templates/material', function (req, res) {
+  res.render('templates/material/index', {
+    channel: 'Express',
+    title: 'dEspadas Boilerplate - bootstrap templates'
+  })
+})
 
-app.listen(3000);
+app.use(helmet())
+
+app.use('/css', express.static(path.join(__dirname, 'tmp/css')))
+app.use('/js', express.static(path.join(__dirname, 'tmp/js')))
+app.use('/libs', express.static(path.join(__dirname, 'tmp/libs')))
+app.use('/fonts', express.static(path.join(__dirname, 'tmp/fonts')))
+
+// app.use(serveStatic('public'))
+
+app.listen(3000, function () {
+  console.log('Started server in port 3000')
+  console.log('Env: ' + process.env.NODE_ENV)
+})
